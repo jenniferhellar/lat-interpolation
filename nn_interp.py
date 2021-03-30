03903c23d2db0b0cc24aa48f9fe6333ca2f96f9d
Hellar, Jennifer <jenniferhellar@gmail.com>
2021-03-30 16:26:31 -0700

    Work on improving different interpolation schemes.  Added SNR and WMSE calculations.  Experimenting with GPR functions

--- nn_interp.py
+++ nn_interp.py
@@ -23,10 +23,8 @@ from readMesh import readMesh
 from readLAT import readLAT
 
 
-from utils import *
-
-
-WRAP				=		0
+WRAP			=		0
+WEIGHTED_MSE	=		0
 
 
 dataDir = 'data/'
@@ -68,54 +66,13 @@ SAMP_LAT = [LAT[i] for i in range(N) if IS_SAMP[i] is True]
 M = len(SAMP_IDX)
 
 
-"""
-Get edges and corresponding adjacent triangles.
-
-edges: list of edges, edge = set(v_i, v_j)
-triangles: list of corr. triangles adj to each edge, tri = (v_i, v_j, v_k)
-"""
-print('Generating edge matrix ...')
-[EDGES, TRI] = getE(connectivityMatrix)
-
-
-""" 
-A: NxN unweighted adjacency matrix
-W: NxN weighted adjacency matrix (cotan weights)
-D: NxN degree matrix
-I: NxN identity matrix 
-
-L: NxN Laplace matrix
-
-"""
-print('Calculating adjacency matrices ...')
-A = getUnWeightedAdj(coordinateMatrix, EDGES, TRI)
-W = getAdjMatrix(coordinateMatrix, EDGES, TRI)
-
-D = np.diag(A.sum(axis=1))
-I = np.identity(N)
-
-print('Calculating Laplacian matrix ...')
-# L = D - W
-L = D - A
-
-
-""" Hyperparameters """
-alpha = 0.1
-beta = 1
-
-
 """ Cross-validation """
 
 folds = 10
 kf12 = KFold(n_splits=folds, shuffle=True)
 
 fold = 0
-
-y = np.zeros((N,1))
-M_l = np.zeros((N,N))
-M_u = np.zeros((N,N))
-
-yhat = np.zeros((N,1))
+yhatNN = np.zeros((N,1))
 
 for tr_i, tst_i in kf12.split(SAMP_IDX):
 	# number of labelled and unlabelled vertices in this fold
@@ -136,30 +93,19 @@ for tr_i, tst_i in kf12.split(SAMP_IDX):
 	TrVal = [LAT[i] for i in TrIdx]
 	TstVal = [LAT[i] for i in TstIdx]
 
-	# Compute graph interpolation estimate for unlabelled vertices in this fold
-	for i in range(N):
-		if i in TrIdx:
-			y[i] = LAT[i]
-			M_l[i,i] = float(1)
-		else:
-			M_u[i,i] = float(1)
-
-			
-	T = np.linalg.inv(M_l + alpha*M_u + beta*L)
-
-	yhatFold = np.matmul(T, y)
-
-	yhatFold = yhatFold[TstIdx]
+	# Compute nearest neighbor estimate for unlabelled vertices in this fold
+	yhatNNfold = griddata(np.array(TrCoord), np.array(TrVal), np.array(TstCoord), method='nearest')
 
 	# Calculate the mean squared error for this fold
-	mse = 0
+	mseNN = 0
 	for i in range(tstLen):
 		# vertex index
 		verIdx = TstIdx[i]
 
-		latEst = yhatFold[i]
+		latEst = yhatNNfold[i]
 		latTrue = LAT[verIdx]
 
+		# calculate error
 		if WRAP:
 			err = abs(latEst - latTrue)
 			rng = max(TrVal) - min(TrVal)
@@ -168,33 +114,31 @@ for tr_i, tst_i in kf12.split(SAMP_IDX):
 			err = abs(latEst - latTrue)
 
 		# save the estimated value and error for this vertex
-		yhat[verIdx] = latEst
+		yhatNN[verIdx] = latEst
 
 		# accumulate squared error
-		mse += (err ** 2)
+		mseNN += (err ** 2)
 	# average the squared error
-	mse = mse/tstLen
-	print('Graph-Estimate MSE:\t' + str(mse))
+	mseNN = mseNN/tstLen
+	print('NN-Estimate MSE:\t' + str(mseNN))
 
 	fold += 1
 
-# errVec = np.array([yhat[i] - LAT[i] for i in range(N) if IS_SAMP[i] is True])
-
 if WRAP:
 	rng = max(LAT)-min(LAT)
-	errVec = [yhat[i]-LAT[i] for i in range(N) if IS_SAMP[i] is True]
+	errVec = [yhatNN[i]-LAT[i] for i in range(N) if IS_SAMP[i] is True]
 	errVec = abs(np.array(errVec))
-	errVec = [min(errVec[i], rng - errVec[i]) for i in range(M)]
-	errVec = np.array(errVec)
+	errVecNN = [min(errVec[i], rng - errVec[i]) for i in range(M)]
+	errVecNN = np.array(errVecNN)
 else:
-	Vec = [abs(yhat[i] - LAT[i]) for i in range(N) if IS_SAMP[i] is True]
-	# mxerr = (round(max(Vec)[0]/10)+1)*10
-	# n, bins = np.histogram(Vec, range=(0, mxerr))
+	Vec = [abs(yhatNN[i] - LAT[i]) for i in range(N) if IS_SAMP[i] is True]
+	# mxerr = (round(max(errVec)[0]/10)+1)*10
+	# n, bins = np.histogram(errVec, range=(0, mxerr))
 	n, bins = np.histogram(Vec, bins=25, range=(0, 250))
 	# print(bins)
 	freq = n/sum(n)
 
-	errVecW = []
+	errVecNN = []
 	for i in range(M):
 		elem = Vec[i]
 		for j, val in enumerate(bins):
@@ -202,27 +146,25 @@ else:
 				idx = j - 1
 				break
 		weightedErr = elem*freq[idx]
-		errVecW.append(weightedErr)
-	errVecW = np.array(errVecW)
-	errVec = np.array(Vec)
+		errVecNN.append(weightedErr)
+	errVecWNN = np.array(errVecNN)
+	errVecNN = np.array(Vec)
 
-avgMSE = 1/M*np.sum(errVec ** 2)
-avgWMSE = 1/M*np.sum(errVecW ** 2)
+avgMSE = 1/M*np.sum(errVecNN ** 2)
+avgWMSE = 1/M*np.sum(errVecWNN ** 2)
 
 print('\n\nMSE:\t' + str(avgMSE))
 print('WMSE:\t' + str(avgWMSE))
 
-snr = 20*np.log10(np.sum(np.array(SAMP_LAT) ** 2)/(M*avgMSE))
-snrW = 20*np.log10(np.sum(np.array(SAMP_LAT) ** 2)/(M*avgWMSE))	# https://doi.org/10.1190/geo2017-0495.1, https://arxiv.org/pdf/1511.05440.pdf, https://arxiv.org/abs/1804.06919v1
-"""
-Bao, Wenbo, et al. "Depth-aware video frame interpolation." Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition. 2019.
-"""
+snr = 20*np.log10(np.sum(np.array(SAMP_LAT) ** 2)/np.sum(errVecNN ** 2))
+snrW = 20*np.log10(np.sum(np.array(SAMP_LAT) ** 2)/np.sum(errVecWNN ** 2))
+
 print('\n\nSNR:\t' + str(snr))
 print('WSNR:\t' + str(snrW))
 
-# print('\n\nFraction of total with <15ms estimation error:\t' + str(np.sum(abs(errVec) < 15)/M))
-# print('Fraction of total with <10ms estimation error:\t' + str(np.sum(abs(errVec) < 10)/M))
-# print('Fraction of total with <5ms estimation error:\t' + str(np.sum(abs(errVec) < 5)/M))
+# print('\n\nFraction of total with <15ms error:\t' + str(np.sum(abs(errVecNN) < 15)/M))
+# print('Fraction of total with <10ms error:\t' + str(np.sum(abs(errVecNN) < 10)/M))
+# print('Fraction of total with <5ms error:\t' + str(np.sum(abs(errVecNN) < 5)/M))
 
 x = [i for i in range(M)]
 
@@ -230,24 +172,24 @@ fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize=(16,8))
 axes = ax.flatten()
 
 thisAx = axes[0]
-thisAx.scatter(x, errVec)
+thisAx.scatter(x, errVecNN)
 thisAx.set_title('|Error|')
 thisAx.set_xlabel('Vertex')
 thisAx.set_ylabel('Error (ms)')
 
 thisAx = axes[1]
-thisAx.scatter(x, errVecW)
+thisAx.scatter(x, errVecWNN)
 thisAx.set_title('|Error|*(frequency of error)')
 thisAx.set_xlabel('Vertex')
 thisAx.set_ylabel('Error Weighted by Occurrence Frequency\nbin width='+str(250/(len(bins)-1))+'ms')
 	
 plt.show()
 
-# n, bins, patches = plt.hist(x=errVec, bins='auto', color='#0504aa', rwidth=0.85)
+# n, bins, patches = plt.hist(x=errVecNN, bins='auto', color='#0504aa', rwidth=0.85)
 # plt.grid(axis='y', alpha=0.75)
 # plt.xlabel('Error')
 # plt.ylabel('Frequency')
-# plt.title('Graph Estimation Error Histogram')
+# plt.title('NN Estimation Error Histogram')
 # # plt.text(23, 45, r'$\alpha=0.1, \beta=1$')
 # maxfreq = n.max()
 # # Set a clean upper y-axis limit.
@@ -265,7 +207,6 @@ axes = ax.flatten()
 # Plot true LAT signal
 thisAx = axes[0]
 thisAx.plot_trisurf(triang, coordinateMatrix[:,2], color='grey', alpha=0.2)
-# thisAx.plot_trisurf(triang, coordinateMatrix[:,2], color='grey')
 pos = thisAx.scatter(pltCoord[:,0], pltCoord[:,1], pltCoord[:,2], c=SAMP_LAT, cmap='rainbow_r', vmin=-200, vmax=50, s = 20)
 
 thisAx.set_title('LAT Signal (True)')
@@ -276,19 +217,11 @@ cax = fig.add_axes([thisAx.get_position().x0+0.015,thisAx.get_position().y0-0.05
 plt.colorbar(pos, cax=cax, label='LAT (ms)', orientation="horizontal")
 
 # Plot overall estimated LAT signal (aggregated from computation in each fold)
-out = yhat[SAMP_IDX]
-# pltSig = [out[i] for i in range(len(out)) if out[i] < 50 and out[i] > -200]
-# pltCoord = [SAMP_COORD[i] for i in range(len(out)) if out[i] < 50 and out[i] > -200]
-pltSig = out
-pltCoord = SAMP_COORD
-pltCoord = np.array(pltCoord)
-
-
 thisAx = axes[1]
 thisAx.plot_trisurf(triang, coordinateMatrix[:,2], color='grey', alpha=0.2)
-pos = thisAx.scatter(pltCoord[:,0], pltCoord[:,1], pltCoord[:,2], c=pltSig, cmap='rainbow_r', vmin=-200, vmax=50, s = 20)
+pos = thisAx.scatter(pltCoord[:,0], pltCoord[:,1], pltCoord[:,2], c=yhatNN[SAMP_IDX], cmap='rainbow_r', vmin=-200, vmax=50, s = 20)
 
-thisAx.set_title('LAT Signal (Graph Estimation)')
+thisAx.set_title('LAT Signal (Estimated)')
 thisAx.set_xlabel('X', fontweight ='bold') 
 thisAx.set_ylabel('Y', fontweight ='bold') 
 thisAx.set_zlabel('Z', fontweight ='bold')
