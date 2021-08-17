Fixed the normalization in deltaE computation, removed dE1976 (outdated), moved metric functions to metrics.py, and modified the edge removal algorithm to only remove if known point nearby.

magicLAT.py
@@ -1,17 +1,15 @@
 
 import numpy as np
-
 import math
-# plotting packages
-import matplotlib.pyplot as plt
-import matplotlib.tri as mtri
-
-from numpy.linalg import norm
 
 import robust_laplacian
 
 # nearest-neighbor interpolation
 from scipy.interpolate import griddata
+
+# KD-Tree for mapping to nearest point
+from scipy.spatial import cKDTree
+
 
 
 def edgeMatrix(coordinateMatrix, connectivityMatrix):
@@ -62,29 +60,49 @@ def edgeMatrix(coordinateMatrix, connectivityMatrix):
 	return [np.array(edges), triangles]
 
 
+def updateEdges(V, E, latTiled, knownV, thresh, returnMidpoints=False):
+	newE = []
 
-def updateEdges(coordinateMatrix, edges, lat, thresh):
-	newEdges = []
+	if returnMidpoints:
+		excl_midpt = []
 
-	excl_midpt = []
+	# KD Tree to find the nearest known mesh vertex
+	coordKDtree = cKDTree(knownV)
 
-	for i in range(len(edges)):
-		e = list(edges[i])
-		v_i = e[0]
+	for i in range(len(E)):
+		e = list(E[i])
+		v_i = e[0]	# vertex indices
 		v_j = e[1]
-		lat_i = lat[v_i]
-		lat_j = lat[v_j]
+		lat_i = latTiled[v_i]	# lat values (tiled manifold)
+		lat_j = latTiled[v_j]
 
-		if abs(lat_j - lat_i) < thresh:
-			newEdges.append(e)
+		[x1, y1, z1] = V[v_i, :]	# vertex coordinates
+		[x2, y2, z2] = V[v_j, :]
+
+		# find distance from nearest known point to vertex i
+		[di, ni] = coordKDtree.query([x1, y1, z1], k=2)
+		if di[0] > 0:
+			di = di[0]
 		else:
-			[x1, y1, z1] = coordinateMatrix[v_i, :]
-			[x2, y2, z2] = coordinateMatrix[v_j, :]
+			di = di[1]	# first point found may be itself
+		# find distance from nearest known point to vertex j
+		[dj, nj] = coordKDtree.query([x2, y2, z2], k=2)
+		if dj[0] > 0:
+			dj = dj[0]
+		else:
+			dj = dj[1]	# first point found may be itself
+
+		if (abs(lat_j - lat_i) < thresh) or (di > 15) or (dj > 15):
+		# if (abs(lat_j - lat_i) < thresh):
+			newE.append(e)
+		elif returnMidpoints:
 			[x, y, z] = [float(x1+x2)/2, float(y1+y2)/2, float(z1+z2)/2]
 			excl_midpt.append([x, y, z])
 
-	return [np.array(newEdges), np.array(excl_midpt)]
-
+	if returnMidpoints:
+		return [np.array(newE), np.array(excl_midpt)]
+	else:
+		return np.array(newE)
 
 
 def getUnWeightedAdj(n, edges):
@@ -101,7 +119,6 @@ def getUnWeightedAdj(n, edges):
 		A[v_j, v_i] = 1
 
 	return A
-
 
 
 def magicLAT(V, F, E, trIdx, trCoord, trLAT, edgeThreshold=50, alpha=5e-05, beta=1):
@@ -136,7 +153,7 @@ def magicLAT(V, F, E, trIdx, trCoord, trLAT, edgeThreshold=50, alpha=5e-05, beta
 		else:
 			latNN[i] = lat[i]
 
-	[edges, excl_midpt] = updateEdges(V, E, latNN, edgeThreshold)
+	edges = updateEdges(V, E, latNN, trCoord, edgeThreshold)
 
 	A = getUnWeightedAdj(N, edges)
 

metrics_nonuniform_sampling.py
@@ -9,9 +9,6 @@ import numpy as np
 import math
 import random
 
-# plotting packages
-from vedo import *
-
 # Gaussian process regression interpolation
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
@@ -23,6 +20,7 @@ from readLAT import readLAT
 
 from utils import *
 from const import *
+from metrics import *
 from magicLAT import *
 
 from quLATiHelper import *
@@ -34,9 +32,10 @@ p034 = 14
 p035 = 18
 p037 = 21
 """
-PATIENT_MAP				=		18
+PATIENT_MAP				=		21
 
 NUM_TRAIN_SAMPS 		= 		100
+
 EDGE_THRESHOLD			=		50
 
 outDir				 	=		'results_wip'
@@ -189,9 +188,9 @@ mae = calcMAE(TstVal, latEst[TstIdx])
 maeGPR = calcMAE(TstVal, latEstGPR[TstIdx])
 maequLATi = calcMAE(TstVal, latEstquLATi[TstIdx])
 
-dE1976, dE2000 = deltaE(TstVal, latEst[TstIdx], MINLAT, MAXLAT)
-dE1976GPR, dE2000GPR = deltaE(TstVal, [latEstGPR[TstIdx]], MINLAT, MAXLAT)
-dE1976quLATi, dE2000quLATi = deltaE(TstVal, [latEstquLATi[TstIdx]], MINLAT, MAXLAT)
+dE = deltaE(TstVal, latEst[TstIdx], MINLAT, MAXLAT)
+dEGPR = deltaE(TstVal, latEstGPR[TstIdx], MINLAT, MAXLAT)
+dEquLATi = deltaE(TstVal, latEstquLATi[TstIdx], MINLAT, MAXLAT)
 
 # magic_spatio_corr = []
 # gpr_spatio_corr = []
@@ -250,5 +249,4 @@ with open(os.path.join(outDir, 'metrics.txt'), 'w') as fid:
 	fid.write('\nColor-Based\n')
 	# fid.write('{:<20}{:<20.6f}{:<20.6f}{:<20.6f}\n'.format('Histogram Corr.', np.mean(magic_corr), np.mean(gpr_corr), np.mean(quLATi_corr)))
 	# fid.write('{:<20}{:<20.6f}{:<20.6f}{:<20.6f}\n'.format('Spatiogram Corr.', np.mean(magic_spatio_corr), np.mean(gpr_spatio_corr), np.mean(quLATi_spatio_corr)))
-	fid.write('{:<20}{:<20.6f}{:<20.6f}{:<20.6f}\n'.format('DeltaE-1976', dE1976, dE1976GPR, dE1976quLATi))
-	fid.write('{:<20}{:<20.6f}{:<20.6f}{:<20.6f}\n'.format('DeltaE-2000', dE2000, dE2000GPR, dE2000quLATi))
+	fid.write('{:<20}{:<20.6f}{:<20.6f}{:<20.6f}\n'.format('DeltaE-2000', dE, dEGPR, dEquLATi))

metrics_repeated_nonuniform.py
@@ -9,9 +9,6 @@ import numpy as np
 import math
 import random
 
-# plotting packages
-from vedo import *
-
 # Gaussian process regression interpolation
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
@@ -23,6 +20,7 @@ from readLAT import readLAT
 
 from utils import *
 from const import *
+from metrics import *
 from magicLAT import *
 
 from quLATiHelper import *
@@ -109,24 +107,21 @@ sampLst = getModifiedSampList(latVals)
 
 magicNMSE = [0 for i in range(NUM_TEST_REPEATS)]
 magicMAE = [0 for i in range(NUM_TEST_REPEATS)]
-magicHistCorr = [0 for i in range(NUM_TEST_REPEATS)]
-magicSpatioCorr = [0 for i in range(NUM_TEST_REPEATS)]
-magicDE1976 = [0 for i in range(NUM_TEST_REPEATS)]
-magicDE2000 = [0 for i in range(NUM_TEST_REPEATS)]
+# magicHistCorr = [0 for i in range(NUM_TEST_REPEATS)]
+# magicSpatioCorr = [0 for i in range(NUM_TEST_REPEATS)]
+magicDE = [0 for i in range(NUM_TEST_REPEATS)]
 
 gprNMSE = [0 for i in range(NUM_TEST_REPEATS)]
 gprMAE = [0 for i in range(NUM_TEST_REPEATS)]
-gprHistCorr = [0 for i in range(NUM_TEST_REPEATS)]
-gprSpatioCorr = [0 for i in range(NUM_TEST_REPEATS)]
-gprDE1976 = [0 for i in range(NUM_TEST_REPEATS)]
-gprDE2000 = [0 for i in range(NUM_TEST_REPEATS)]
+# gprHistCorr = [0 for i in range(NUM_TEST_REPEATS)]
+# gprSpatioCorr = [0 for i in range(NUM_TEST_REPEATS)]
+gprDE = [0 for i in range(NUM_TEST_REPEATS)]
 
 quLATiNMSE = [0 for i in range(NUM_TEST_REPEATS)]
 quLATiMAE = [0 for i in range(NUM_TEST_REPEATS)]
-quLATiHistCorr = [0 for i in range(NUM_TEST_REPEATS)]
-quLATiSpatioCorr = [0 for i in range(NUM_TEST_REPEATS)]
-quLATiDE1976 = [0 for i in range(NUM_TEST_REPEATS)]
-quLATiDE2000 = [0 for i in range(NUM_TEST_REPEATS)]
+# quLATiHistCorr = [0 for i in range(NUM_TEST_REPEATS)]
+# quLATiSpatioCorr = [0 for i in range(NUM_TEST_REPEATS)]
+quLATiDE = [0 for i in range(NUM_TEST_REPEATS)]
 
 # For colorbar ranges
 MINLAT = math.floor(min(allLatVal)/10)*10
@@ -173,8 +168,8 @@ for test in range(NUM_TEST_REPEATS):
 	Create images for visual metrics
 	"""
 
-	createTestPointImages(vertices, faces, MINLAT, MAXLAT, outDir, 
-	TstCoord, TstVal, latEst[TstIdx], latEstGPR[TstIdx], latEstquLATi[TstIdx])
+	# createTestPointImages(vertices, faces, MINLAT, MAXLAT, outDir, 
+	# TstCoord, TstVal, latEst[TstIdx], latEstGPR[TstIdx], latEstquLATi[TstIdx])
 
 
 	"""
@@ -189,81 +184,78 @@ for test in range(NUM_TEST_REPEATS):
 	maeGPR = calcMAE(TstVal, latEstGPR[TstIdx])
 	maequLATi = calcMAE(TstVal, latEstquLATi[TstIdx])
 
-	dE1976, dE2000 = deltaE(TstVal, latEst[TstIdx], MINLAT, MAXLAT)
-	dE1976GPR, dE2000GPR = deltaE(TstVal, [latEstGPR[TstIdx]], MINLAT, MAXLAT)
-	dE1976quLATi, dE2000quLATi = deltaE(TstVal, [latEstquLATi[TstIdx]], MINLAT, MAXLAT)
+	dE = deltaE(TstVal, latEst[TstIdx], MINLAT, MAXLAT)
+	dEGPR = deltaE(TstVal, latEstGPR[TstIdx], MINLAT, MAXLAT)
+	dEquLATi = deltaE(TstVal, latEstquLATi[TstIdx], MINLAT, MAXLAT)
 
-	magic_spatio_corr = []
-	gpr_spatio_corr = []
-	quLATi_spatio_corr = []
+	# magic_spatio_corr = []
+	# gpr_spatio_corr = []
+	# quLATi_spatio_corr = []
 
-	magic_corr = []
-	gpr_corr = []
-	quLATi_corr = []
+	# magic_corr = []
+	# gpr_corr = []
+	# quLATi_corr = []
 
-	bins = 16
-	binEdges = [i*256/bins for i in range(bins+1)]
+	# bins = 16
+	# binEdges = [i*256/bins for i in range(bins+1)]
 
-	# four sides of the object
-	elev = 0
-	azim = [0, 90, 180, 270]
+	# # four sides of the object
+	# elev = 0
+	# azim = [0, 90, 180, 270]
 
-	for a in azim:
-		magic_scorr, magic_cor, gpr_scorr, gpr_cor, quLATi_scorr, quLATi_cor = colorHistAndSpatioCorr(outDir, 'true_elev{:g}azim{:g}.png'.format(elev, a),
-			'magic_elev{:g}azim{:g}.png'.format(elev, a),
-			'gpr_elev{:g}azim{:g}.png'.format(elev, a),
-			'quLATi_elev{:g}azim{:g}.png'.format(elev, a),
-			bins, binEdges,
-			'{:g}azim{:g}.png'.format(elev, a))
+	# for a in azim:
+	# 	magic_scorr, magic_cor, gpr_scorr, gpr_cor, quLATi_scorr, quLATi_cor = colorHistAndSpatioCorr(outDir, 'true_elev{:g}azim{:g}.png'.format(elev, a),
+	# 		'magic_elev{:g}azim{:g}.png'.format(elev, a),
+	# 		'gpr_elev{:g}azim{:g}.png'.format(elev, a),
+	# 		'quLATi_elev{:g}azim{:g}.png'.format(elev, a),
+	# 		bins, binEdges,
+	# 		'{:g}azim{:g}.png'.format(elev, a))
 
-		magic_corr.append(magic_cor)
-		gpr_corr.append(gpr_cor)
-		quLATi_corr.append(quLATi_cor)
+	# 	magic_corr.append(magic_cor)
+	# 	gpr_corr.append(gpr_cor)
+	# 	quLATi_corr.append(quLATi_cor)
 
-		magic_spatio_corr.append(magic_scorr)
-		gpr_spatio_corr.append(gpr_scorr)
-		quLATi_spatio_corr.append(quLATi_scorr)
+	# 	magic_spatio_corr.append(magic_scorr)
+	# 	gpr_spatio_corr.append(gpr_scorr)
+	# 	quLATi_spatio_corr.append(quLATi_scorr)
 
-	# top and bottom perspective of the object
-	elev = [-90, 90]
-	azim = 0
+	# # top and bottom perspective of the object
+	# elev = [-90, 90]
+	# azim = 0
 
-	for e in elev:
-		magic_scorr, magic_cor, gpr_scorr, gpr_cor, quLATi_scorr, quLATi_cor = colorHistAndSpatioCorr(outDir, 'true_elev{:g}azim{:g}.png'.format(e, azim),
-			'magic_elev{:g}azim{:g}.png'.format(e, azim),
-			'gpr_elev{:g}azim{:g}.png'.format(e, azim),
-			'quLATi_elev{:g}azim{:g}.png'.format(e, azim),
-			bins, binEdges,
-			'{:g}azim{:g}.png'.format(e, azim))
+	# for e in elev:
+	# 	magic_scorr, magic_cor, gpr_scorr, gpr_cor, quLATi_scorr, quLATi_cor = colorHistAndSpatioCorr(outDir, 'true_elev{:g}azim{:g}.png'.format(e, azim),
+	# 		'magic_elev{:g}azim{:g}.png'.format(e, azim),
+	# 		'gpr_elev{:g}azim{:g}.png'.format(e, azim),
+	# 		'quLATi_elev{:g}azim{:g}.png'.format(e, azim),
+	# 		bins, binEdges,
+	# 		'{:g}azim{:g}.png'.format(e, azim))
 
-		magic_corr.append(magic_cor)
-		gpr_corr.append(gpr_cor)
-		quLATi_corr.append(quLATi_cor)
+	# 	magic_corr.append(magic_cor)
+	# 	gpr_corr.append(gpr_cor)
+	# 	quLATi_corr.append(quLATi_cor)
 
-		magic_spatio_corr.append(magic_scorr)
-		gpr_spatio_corr.append(gpr_scorr)
-		quLATi_spatio_corr.append(quLATi_scorr)
+	# 	magic_spatio_corr.append(magic_scorr)
+	# 	gpr_spatio_corr.append(gpr_scorr)
+	# 	quLATi_spatio_corr.append(quLATi_scorr)
 
 	magicNMSE[test] = nmse
 	magicMAE[test] = mae
-	magicHistCorr[test] = np.mean(magic_corr)	# average the 6 perspectives
-	magicSpatioCorr[test] = np.mean(magic_spatio_corr)
-	magicDE1976[test] = dE1976
-	magicDE2000[test] = dE2000
+	# magicHistCorr[test] = np.mean(magic_corr)	# average the 6 perspectives
+	# magicSpatioCorr[test] = np.mean(magic_spatio_corr)
+	magicDE[test] = dE
 
 	gprNMSE[test] = nmseGPR
 	gprMAE[test] = maeGPR
-	gprHistCorr[test] = np.mean(gpr_corr)
-	gprSpatioCorr[test] = np.mean(gpr_spatio_corr)
-	gprDE1976[test] = dE1976GPR
-	gprDE2000[test] = dE2000GPR
+	# gprHistCorr[test] = np.mean(gpr_corr)
+	# gprSpatioCorr[test] = np.mean(gpr_spatio_corr)
+	gprDE[test] = dEGPR
 
 	quLATiNMSE[test] = nmsequLATi
 	quLATiMAE[test] = maequLATi
-	quLATiHistCorr[test] = np.mean(quLATi_corr)
-	quLATiSpatioCorr[test] = np.mean(quLATi_spatio_corr)
-	quLATiDE1976[test] = dE1976quLATi
-	quLATiDE2000[test] = dE2000quLATi
+	# quLATiHistCorr[test] = np.mean(quLATi_corr)
+	# quLATiSpatioCorr[test] = np.mean(quLATi_spatio_corr)
+	quLATiDE[test] = dEquLATi
 
 
 filename = os.path.join(outDir, 'p{}_t{:g}_m{:g}_tests{:g}.txt'.format(patient, EDGE_THRESHOLD, NUM_TRAIN_SAMPS, NUM_TEST_REPEATS))
@@ -281,27 +273,24 @@ with open(filename, 'w') as fid:
 	fid.write('MAGIC-LAT Performance\n\n')
 	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('NMSE', 	np.average(magicNMSE), 	np.std(magicNMSE)))
 	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('MAE', 	np.average(magicMAE), 	np.std(magicMAE)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Histogram Corr.', 	np.average(magicHistCorr), 	np.std(magicHistCorr)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Spatiogram Corr.', 	np.average(magicSpatioCorr), np.std(magicSpatioCorr)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-1976', 	np.average(magicDE1976), np.std(magicDE1976)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-2000', 	np.average(magicDE2000), np.std(magicDE2000)))
+	# fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Histogram Corr.', 	np.average(magicHistCorr), 	np.std(magicHistCorr)))
+	# fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Spatiogram Corr.', 	np.average(magicSpatioCorr), np.std(magicSpatioCorr)))
+	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-2000', 	np.average(magicDE), np.std(magicDE)))
 
 	fid.write('\n\n')
 
 	fid.write('GPR Performance\n\n')
 	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('NMSE', 	np.average(gprNMSE), 	np.std(gprNMSE)))
 	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('MAE', 	np.average(gprMAE), 	np.std(gprMAE)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Histogram Corr.', 	np.average(gprHistCorr), 	np.std(gprHistCorr)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Spatiogram Corr.', 	np.average(gprSpatioCorr), 	np.std(gprSpatioCorr)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-1976', 	np.average(gprDE1976), np.std(gprDE1976)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-2000', 	np.average(gprDE2000), np.std(gprDE2000)))
+	# fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Histogram Corr.', 	np.average(gprHistCorr), 	np.std(gprHistCorr)))
+	# fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Spatiogram Corr.', 	np.average(gprSpatioCorr), 	np.std(gprSpatioCorr)))
+	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-2000', 	np.average(gprDE), np.std(gprDE)))
 
 	fid.write('\n\n')
 
 	fid.write('quLATi Performance\n\n')
 	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('NMSE', 	np.average(quLATiNMSE), 	np.std(quLATiNMSE)))
 	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('MAE', 	np.average(quLATiMAE), 	np.std(quLATiMAE)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Histogram Corr.', 	np.average(quLATiHistCorr), 	np.std(quLATiHistCorr)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Spatiogram Corr.', 	np.average(quLATiSpatioCorr), 	np.std(quLATiSpatioCorr)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-1976', 	np.average(quLATiDE1976), np.std(quLATiDE1976)))
-	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-2000', 	np.average(quLATiDE2000), np.std(quLATiDE2000)))
+	# fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Histogram Corr.', 	np.average(quLATiHistCorr), 	np.std(quLATiHistCorr)))
+	# fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('Spatiogram Corr.', 	np.average(quLATiSpatioCorr), 	np.std(quLATiSpatioCorr)))
+	fid.write('{:<30}{:.4f} +/- {:.4f}\n'.format('DeltaE-2000', 	np.average(quLATiDE), np.std(quLATiDE)))

utils.py
@@ -15,218 +15,104 @@ Email: jennifer.hellar@rice.edu
 --------------------------------------------------------------------------------
 """
 
-import matplotlib.pyplot as plt
-import matplotlib.tri as mtri
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from numpy.linalg import norm
 import math
 
 import os
-
-import cv2
 from vedo import *
 
-import matplotlib.cm as cm
-from matplotlib.colors import Normalize
-import colour
-
-# KD-Tree for mapping to nearest point
 from scipy.spatial import cKDTree
 
-
-def findSpatioMeanAndSigma(bins, binEdges, fig, nonEmpty):
-
-	dimX = fig.shape[0]
-	dimY = fig.shape[1]
-
-	mu = np.zeros((bins, bins, bins, 2))
-	sigma = np.zeros((bins, bins, bins, 2, 2))
-
-	for r_i, r_v in enumerate(binEdges):
-		for g_i, g_v in enumerate(binEdges):
-			for b_i, b_v in enumerate(binEdges):
-				if r_v < 256 and g_v < 256 and b_v < 256:
-					if nonEmpty[r_i, g_i, b_i]:
-						pxls = np.column_stack(np.where((fig[:, :, 0] >= r_v) & (fig[:, :, 0] < binEdges[r_i + 1]) & \
-							(fig[:, :, 1] >= g_v) & (fig[:, :, 1] < binEdges[g_i + 1]) & \
-							(fig[:, :, 2] >= b_v) & (fig[:, :, 2] < binEdges[b_i + 1])))
-						if pxls.shape[0] == 0:
-							mu[r_i, g_i, b_i] = np.array([[dimX/2, dimY/2]])
-							sigma[r_i, g_i, b_i] = np.array([[1, 0], [0, 1]])
-						else:
-							mu[r_i, g_i, b_i] = np.mean(pxls, axis=0)
-							sigma[r_i, g_i, b_i] = np.cov(pxls, rowvar=0)
-	return mu, sigma
+import matplotlib.pyplot as plt
 
 
-def js(mu1, sigma1, n1, mu2, sigma2, n2):
-	if (n1 == 0 and n2 == 0):
-		return 0
 
-	mu_hat = (n2*mu1 + n1*mu2)/(n1 + n2)
+def mapSamps(IDX, COORD, coords, vals):
+	"""
+	Maps LAT sample values (vals) at coordinates (coords) not
+	on the mesh to the nearest mesh coordinate in COORD.
 
-	sigma_hat = n2/(n1+n2) * (sigma1 + np.matmul(mu1, mu1.T)) + \
-		n1/(n1+n2) * (sigma2 + np.matmul(mu2, mu2.T)) - np.matmul(mu_hat, mu_hat.T)
+	Returns:
+	- latIdx, a list of mesh vertex indices with an LAT sample
+	- latCoords, a list of corr. LAT vertex coordinates
+	- latVals, a list of corr. LAT values
 
-	if np.linalg.det(sigma1) <= 0 or np.linalg.det(sigma2) <= 0 or np.linalg.det(sigma_hat) <= 0:
-		return 0
+	"""
+	n = len(IDX)	# number of vertices in the graph
+	m = len(coords)			# number of signal samples
 
-	t0 = math.log(np.linalg.det(sigma_hat))
+	# KD Tree to find the nearest mesh vertex
+	coordKDtree = cKDTree(COORD)
+	[_, nearestVer] = coordKDtree.query(coords, k=1)
 
-	t1 = 1/2 * np.trace(np.matmul(np.linalg.inv(sigma_hat), (sigma1 + sigma2)))
+	# find the vertices with an assigned (known) sample
+	known = [False for i in range(n)]
+	lat = [0 for i in range(n)]
+	for sampPt in range(m):
+		verIdx = nearestVer[sampPt]	# nearest vertex
+		known[verIdx] = True
+		lat[verIdx] = vals[sampPt]	# assign the value
 
-	t2 = 1/4 * np.matmul((mu1 - mu2).T, np.matmul(np.linalg.inv(sigma_hat), (mu1 - mu2)))
+	latIdx = [IDX[i] for i in range(n) if known[i] is True]
+	latCoords = [COORD[i] for i in range(n) if known[i] is True]
+	latVals = [lat[i] for i in range(n) if known[i] is True]
 
-	t3 = 1/2 * math.log(np.linalg.det(sigma1) * np.linalg.det(sigma2))
-
-	js_div = t0 + t1 - 2 + t2 - t3
-
-	return js_div
+	return latIdx, latCoords, latVals
 
 
-def spatioCorr(bins, nonEmpty, trueMean, trueSigma, trueHist, 
-	estMean, estSigma, estHist):
-	spatcorr = 0
+def isAnomalous(allLatCoord, allLatVal, k=6, d=5, thresh=50):
+	# KD Tree to find the nearest mesh vertex
+	coordKDtree = cKDTree(allLatCoord)
+	[dist, nearestVers] = coordKDtree.query(allLatCoord, k=k)
 
-	for i in range(bins):
-		for j in range(bins):
-			for k in range(bins):
-				if nonEmpty[i,j,k]:
-					estjs = js(trueMean[i,j,k], trueSigma[i,j,k], trueHist[i,j,k], 
-						estMean[i,j,k], estSigma[i,j,k], estHist[i,j,k])
+	M = len(allLatCoord)
+	anomalous = np.zeros(M)
 
-					# Histogram correlation
-					num = trueHist[i,j,k] * estHist[i,j,k]
-					denom = math.sqrt(np.sum(trueHist ** 2) * np.sum(estHist ** 2))
-					spatcorr += (num / denom) * math.exp(-1 * estjs)
+	for i in range(M):
+		verCoord = allLatCoord[i]
+		verVal = allLatVal[i]
 
-					# # Histogram intersection
-					# spatcorr += min(trueHist[i, j, k], estHist[i, j, k])*math.exp(-1 * estjs)
+		neighbors = [nearestVers[i, n] for n in range(1,k) if dist[i,n] < d]
 
-	return spatcorr
+		adj = len(neighbors)
 
-def colorHistAndSpatioCorr(outDir, trueImageFile,
-		magicImageFile, gprImageFile, quLATiImageFile,
-		bins, binEdges, outputFileSuffix):
-	img = cv2.imread(os.path.join(outDir, trueImageFile), cv2.IMREAD_GRAYSCALE)
-	n_black_px = np.sum(img == 0)
-	# numpx = np.sum(img > 0)
+		cnt = 0
+		for neighVer in neighbors:
+			neighVal = allLatVal[neighVer]
 
-	figTruth = cv2.imread(os.path.join(outDir, trueImageFile))
-	figTruth = cv2.cvtColor(figTruth, cv2.COLOR_BGR2RGB)
-	
-	true_hist = cv2.calcHist([figTruth], [0, 1, 2], None, [bins, bins, bins],
-		[0, 256, 0, 256, 0, 256])
-	true_hist[0, 0, 0] -= n_black_px
-	cv2.normalize(true_hist, true_hist)
-	true_hist_flat = true_hist.flatten()
+			if abs(verVal - neighVal) > thresh:
+				cnt += 1
+			else:
+				break
 
-	figEst = cv2.imread(os.path.join(outDir, magicImageFile))
-	figEst = cv2.cvtColor(figEst, cv2.COLOR_BGR2RGB)
-	
-	magic_hist = cv2.calcHist([figEst], [0, 1, 2], None, [bins, bins, bins],
-		[0, 256, 0, 256, 0, 256])
-	magic_hist[0, 0, 0] -= n_black_px
-	cv2.normalize(magic_hist, magic_hist)
-	magic_hist_flat = magic_hist.flatten()
+		# if (cnt >= (len(neighbors)-1) and len(neighbors) > 1):	# differs from all but 1 neighbor by >50ms and has at least 2 neighbors w/in 5mm
+		if cnt > 1 and adj > 1:
+			anomalous[i] = 1
+			# print(cnt, adj)
 
-	figEstGPR = cv2.imread(os.path.join(outDir, gprImageFile))
-	figEstGPR = cv2.cvtColor(figEstGPR, cv2.COLOR_BGR2RGB)
-	
-	gpr_hist = cv2.calcHist([figEstGPR], [0, 1, 2], None, [bins, bins, bins],
-		[0, 256, 0, 256, 0, 256])
-	gpr_hist[0, 0, 0] -= n_black_px
-	cv2.normalize(gpr_hist, gpr_hist)
-	gpr_hist_flat = gpr_hist.flatten()
+			# print(verVal, [allLatVal[neighVer] for neighVer in neighbors])
+	return anomalous
 
-	figEstquLATi = cv2.imread(os.path.join(outDir, quLATiImageFile))
-	figEstquLATi = cv2.cvtColor(figEstquLATi, cv2.COLOR_BGR2RGB)
-	
-	quLATi_hist = cv2.calcHist([figEstquLATi], [0, 1, 2], None, [bins, bins, bins],
-		[0, 256, 0, 256, 0, 256])
-	quLATi_hist[0, 0, 0] -= n_black_px
-	cv2.normalize(quLATi_hist, quLATi_hist)
-	quLATi_hist_flat = quLATi_hist.flatten()
 
-	nonEmpty = np.zeros((bins, bins, bins))
-	for r_i, r_v in enumerate(binEdges):
-		for g_i, g_v in enumerate(binEdges):
-			for b_i, b_v in enumerate(binEdges):
-				if r_v < 256 and g_v < 256 and b_v < 256:
-					if true_hist[r_i, g_i, b_i] > 0 or magic_hist[r_i, g_i, b_i] > 0 or gpr_hist[r_i, g_i, b_i] > 0:
-						nonEmpty[r_i, g_i, b_i] = True
+def getModifiedSampList(latVals):
+	M = len(latVals)
 
-	true_mean, true_sigma = findSpatioMeanAndSigma(bins, binEdges, figTruth, nonEmpty)
-	magic_mean, magic_sigma = findSpatioMeanAndSigma(bins, binEdges, figEst, nonEmpty)
-	gpr_mean, gpr_sigma = findSpatioMeanAndSigma(bins, binEdges, figEstGPR, nonEmpty)
-	quLATi_mean, quLATi_sigma = findSpatioMeanAndSigma(bins, binEdges, figEstquLATi, nonEmpty)
+	sort_index = np.argsort(np.array(latVals))
+	sortedLATVals = [latVals[i] for i in sort_index]
+	pos = [int(abs(sortedLATVals[i] - max(sortedLATVals))) for i in range(M)]
+	ratio = [(pos[i] / math.gcd(*pos)) for i in range(M)]
 
-	plt.subplot(421, title='Ground truth image'), plt.imshow(figTruth)
-	plt.subplot(422, title='Ground truth color histograms'),
-	plt.plot(np.sum(np.sum(true_hist, axis=1), axis=1), 'r')
-	plt.plot(np.sum(np.sum(true_hist, axis=0), axis=1), 'g')
-	plt.plot(np.sum(np.sum(true_hist, axis=0), axis=0), 'b')
-	plt.xlim([0,bins])
-	plt.subplot(423, title='MAGIC-LAT image'), plt.imshow(figEst)
-	plt.subplot(424, title='MAGIC-LAT color histograms'),
-	plt.plot(np.sum(np.sum(magic_hist, axis=1), axis=1), 'r')
-	plt.plot(np.sum(np.sum(magic_hist, axis=0), axis=1), 'g')
-	plt.plot(np.sum(np.sum(magic_hist, axis=0), axis=0), 'b')
-	plt.xlim([0,bins])
-	plt.subplot(425, title='GPR image'), plt.imshow(figEstGPR)
-	plt.subplot(426, title='GPR color histograms'),
-	plt.plot(np.sum(np.sum(gpr_hist, axis=1), axis=1), 'r')
-	plt.plot(np.sum(np.sum(gpr_hist, axis=0), axis=1), 'g')
-	plt.plot(np.sum(np.sum(gpr_hist, axis=0), axis=0), 'b')
-	plt.xlim([0,bins])
-	plt.subplot(427, title='quLATi image'), plt.imshow(figEstquLATi)
-	plt.subplot(428, title='quLATi color histograms'),
-	plt.plot(np.sum(np.sum(quLATi_hist, axis=1), axis=1), 'r')
-	plt.plot(np.sum(np.sum(quLATi_hist, axis=0), axis=1), 'g')
-	plt.plot(np.sum(np.sum(quLATi_hist, axis=0), axis=0), 'b')
-	plt.xlim([0,bins])
+	sampLst = []
+	for i in range(M):
+		reps = ratio[i]
+		idx = sort_index[i]
+		if reps == 0.0:
+			sampLst.append(idx)
+		for r in range(int(reps)):
+			sampLst.append(idx)
+	# print(sampLst.count(latVals.index(min(latVals))))
 
-	plt.tight_layout()
-	plt.savefig(os.path.join(outDir, 'colorHist'+outputFileSuffix))
-	# plt.show()
-
-	plt.subplot(421, title='Ground truth image'), plt.imshow(figTruth)
-	plt.subplot(422, title='Ground truth flattened histogram'),
-	plt.plot(true_hist_flat)
-	plt.xlim([0,true_hist_flat.shape[0]])
-	plt.subplot(423, title='MAGIC-LAT image'), plt.imshow(figEst)
-	plt.subplot(424, title='MAGIC-LAT flattened histogram'),
-	plt.plot(magic_hist_flat)
-	plt.xlim([0,true_hist_flat.shape[0]])
-	plt.subplot(425, title='GPR image'), plt.imshow(figEstGPR)
-	plt.subplot(426, title='GPR flattened histogram'),
-	plt.plot(gpr_hist_flat)
-	plt.xlim([0,true_hist_flat.shape[0]])
-	plt.subplot(427, title='quLATi image'), plt.imshow(figEstquLATi)
-	plt.subplot(428, title='quLATi flattened histogram'),
-	plt.plot(quLATi_hist_flat)
-	plt.xlim([0,true_hist_flat.shape[0]])
-
-	plt.tight_layout()
-	plt.savefig(os.path.join(outDir, 'hist'+outputFileSuffix))
-	# plt.show()
-
-	magic_corr = cv2.compareHist(true_hist_flat, magic_hist_flat, cv2.HISTCMP_CORREL)
-	gpr_corr = cv2.compareHist(true_hist_flat, gpr_hist_flat, cv2.HISTCMP_CORREL)
-	quLATi_corr = cv2.compareHist(true_hist_flat, quLATi_hist_flat, cv2.HISTCMP_CORREL)
-
-	magic_spatiocorr = spatioCorr(bins, nonEmpty, true_mean, true_sigma, true_hist, 
-		magic_mean, magic_sigma, magic_hist)
-	
-	gpr_spatiocorr = spatioCorr(bins, nonEmpty, true_mean, true_sigma, true_hist, 
-		gpr_mean, gpr_sigma, gpr_hist)
-
-	quLATi_spatiocorr = spatioCorr(bins, nonEmpty, true_mean, true_sigma, true_hist, 
-		quLATi_mean, quLATi_sigma, quLATi_hist)
-
-	return magic_spatiocorr, magic_corr, gpr_spatiocorr, gpr_corr, quLATi_spatiocorr, quLATi_corr
-
+	return sampLst
 
 
 def createTestPointImages(vertices, faces, MINLAT, MAXLAT, outDir,
@@ -336,227 +222,3 @@ def getPerspective(patient):
 		azimuth = 0
 		roll = 0
 	return elev, azimuth, roll
-
-
-def getModifiedSampList(latVals):
-	M = len(latVals)
-
-	sort_index = np.argsort(np.array(latVals))
-	sortedLATVals = [latVals[i] for i in sort_index]
-	pos = [int(abs(sortedLATVals[i] - max(sortedLATVals))) for i in range(M)]
-	ratio = [(pos[i] / math.gcd(*pos)) for i in range(M)]
-
-	sampLst = []
-	for i in range(M):
-		reps = ratio[i]
-		idx = sort_index[i]
-		if reps == 0.0:
-			sampLst.append(idx)
-		for r in range(int(reps)):
-			sampLst.append(idx)
-	# print(sampLst.count(latVals.index(min(latVals))))
-
-	return sampLst
-
-
-def isAnomalous(allLatCoord, allLatVal, k=6, d=5, thresh=50):
-	# KD Tree to find the nearest mesh vertex
-	coordKDtree = cKDTree(allLatCoord)
-	[dist, nearestVers] = coordKDtree.query(allLatCoord, k=k)
-
-	M = len(allLatCoord)
-	anomalous = np.zeros(M)
-
-	for i in range(M):
-		verCoord = allLatCoord[i]
-		verVal = allLatVal[i]
-
-		neighbors = [nearestVers[i, n] for n in range(1,k) if dist[i,n] < d]
-
-		adj = len(neighbors)
-
-		cnt = 0
-		for neighVer in neighbors:
-			neighVal = allLatVal[neighVer]
-
-			if abs(verVal - neighVal) > thresh:
-				cnt += 1
-			else:
-				break
-
-		# if (cnt >= (len(neighbors)-1) and len(neighbors) > 1):	# differs from all but 1 neighbor by >50ms and has at least 2 neighbors w/in 5mm
-		if cnt > 1 and adj > 1:
-			anomalous[i] = 1
-			# print(cnt, adj)
-
-			# print(verVal, [allLatVal[neighVer] for neighVer in neighbors])
-	return anomalous
-
-
-def delta_e(a, b):
-	"""CIE 1976 """
-	# compute difference
-	diff = cv2.add(a,-b)
-
-	# separate into L,A,B channel diffs
-	diff_L = diff[:,:,0]
-	diff_A = diff[:,:,1]
-	diff_B = diff[:,:,2]
-
-	# compute delta_e as mean over every pixel using equation from  
-	# https://en.wikipedia.org/wiki/Color_difference#CIELAB_ΔE*
-	delta_E = np.mean( np.sqrt(diff_L*diff_L + diff_A*diff_A + diff_B*diff_B) )
-
-	return delta_E
-
-
-def deltaE(trueVals, estVals, MINLAT, MAXLAT, cmap=cm.rainbow_r):
-	norm = Normalize(vmin=MINLAT, vmax=MAXLAT)
-
-	trueColors = cmap(norm(np.array([trueVals])))[:, :, :3]
-	trueColors = cv2.cvtColor(trueColors.astype("float32") / 255, cv2.COLOR_RGB2LAB)
-
-	estColors = cmap(norm(np.array(estVals)))[:, :, :3]
-	estColors = estColors.reshape((1, max(estColors.shape), 3))
-	estColors = cv2.cvtColor(estColors.astype("float32") / 255, cv2.COLOR_RGB2LAB)
-
-	dE1976 = delta_e(trueColors, estColors)
-
-	""" https://colour.readthedocs.io/en/latest/generated/colour.delta_E.html """
-	dE2000 = np.mean(colour.delta_E(trueColors, estColors, method='CIE 2000'))
-
-	return dE1976, dE2000
-
-def mapSamps(IDX, COORD, coords, vals):
-	"""
-	Maps LAT sample values (vals) at coordinates (coords) not
-	on the mesh to the nearest mesh coordinate in COORD.
-
-	Returns:
-	- latIdx, a list of mesh vertex indices with an LAT sample
-	- latCoords, a list of corr. LAT vertex coordinates
-	- latVals, a list of corr. LAT values
-
-	"""
-	n = len(IDX)	# number of vertices in the graph
-	m = len(coords)			# number of signal samples
-
-	# KD Tree to find the nearest mesh vertex
-	coordKDtree = cKDTree(COORD)
-	[_, nearestVer] = coordKDtree.query(coords, k=1)
-
-	# find the vertices with an assigned (known) sample
-	known = [False for i in range(n)]
-	lat = [0 for i in range(n)]
-	for sampPt in range(m):
-		verIdx = nearestVer[sampPt]	# nearest vertex
-		known[verIdx] = True
-		lat[verIdx] = vals[sampPt]	# assign the value
-
-	latIdx = [IDX[i] for i in range(n) if known[i] is True]
-	latCoords = [COORD[i] for i in range(n) if known[i] is True]
-	latVals = [lat[i] for i in range(n) if known[i] is True]
-
-	return latIdx, latCoords, latVals
-
-
-
-
-
-def calcMSE(sig, sigEst):
-	n = len(sig)
-
-	err = [abs(sigEst[i] - sig[i]) for i in range(n)]
-	err = np.array(err)
-
-	mse = 1/n*np.sum(err ** 2)
-
-	return mse
-
-
-def calcMAE(sig, sigEst):
-	delta = [abs(sig[i] - sigEst[i]) for i in range(len(sig))]
-	delta = np.array(delta)
-	return np.average(delta)
-
-
-
-def calcPercError(sig, sigEst):
-	n = len(sig)
-
-	err = [abs(sigEst[i] - sig[i]) for i in range(n)]
-	err = [1 for i in err if i > 10]
-	err = np.array(err)
-
-	return float(np.sum(err))/n
-
-
-def calcNMSE(sig, sigEst, multichannel=False):
-	if multichannel:
-		print(sig.shape, sigEst.shape)
-		err = (np.array(sigEst) - np.array(sig)) ** 2
-		err = np.sum(err, axis=0, keepdims = True)
-		meanvec = np.array(np.mean(sig, axis=0), ndmin=2)
-		# sigPower = np.sum((np.array(sig) - meanvec), axis=0, keepdims = True)
-		sigPower = np.sum(np.array(sig), axis=0, keepdims=True)
-
-		nmse = err / sigPower
-	else:
-		n = len(sig)
-
-		err = [abs(sigEst[i] - sig[i]) for i in range(n)]
-		err = np.array(err)
-
-		sigKnown = [sig[i] for i in range(n)]
-		sigPower = np.sum((np.array(sigKnown) - np.mean(sigKnown)) ** 2)
-
-		nmse = np.sum(err ** 2)/sigPower
-
-	return nmse
-
-
-
-def calcNRMSE(sig, sigEst):
-	n = len(sig)
-
-	err = [abs(sigEst[i] - sig[i]) for i in range(n)]
-	sqErr = np.array(err) ** 2
-
-	rmse = (np.sum(sqErr)/n)**(1/2)
-	nRMSE = 100*rmse/(np.max(sig) - np.min(sig))
-
-	return nRMSE
-
-
-
-def calcSNR(sig, sigEst):
-	n = len(sig)
-	err = [abs(sigEst[i] - sig[i]) for i in range(n)]
-	err = np.array(err)
-
-	sigKnown = [sig[i] for i in range(n)]
-	sigPower = np.sum((np.array(sigKnown) - np.mean(sigKnown)) ** 2)
-
-	snr = 20*np.log10(sigPower/np.sum(err ** 2))
-
-	return snr
-
-
-
-def compute_metrics(sig, sigEst):
-
-	nmse = calcNMSE(sig, sigEst)
-	snr = calcSNR(sig, sigEst)
-	mae = calcMAE(sig, sigEst)
-	nrmse = calcNRMSE(sig, sigEst)
-
-	return nmse, snr, mae, nrmse
-
-
-def pltEigenvalues(lambdas):
-	N = len(lambdas)
-	plt.scatter(range(0, N), lambdas)
-	plt.xlabel('Eigenvalue Index')
-	plt.ylabel('Eigenvalue')
-	plt.title('Graph Laplacian Eigenvalues')
-	plt.show()